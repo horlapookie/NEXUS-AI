9dedd6542f23f27b026da17a4bd56157fa229bfd
Your highness <olamilekanidowu998@gmail.com>
2025-06-22 13:26:29 +0100

    Update set.js

--- set.js
+++ set.js
@@ -14,7 +14,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "2549122222622",
+    OWNER_NAME: process.env.OWNER_NAME || "2349122222622",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
